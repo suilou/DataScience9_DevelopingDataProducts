a51d5ffc88f2e6a0bb5efdaf1b44c7b0978330bf
suilou <shuang@air-worldwide.com>
2016-07-06 00:01:33 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -7,7 +7,7 @@ shinyUI(pageWithSidebar(
     sliderInput('n', 'Window Size (Days)', value = 60, min = 1, max = 360, step = 1),
     br(),
     p(strong(em("Documentation:",a("Moving Average of S&P500",href="https://github.com/suilou/DataScience9_DevelopingDataProducts/blob/master/README.md")))),
-    p(strong(em("Github repository:",a("Developing Data Products - Peer Assessment Project; Shiny App",href="https://github.com/suilou/DataScience9_DevelopingDataProducts"))))
+    p(strong(em("Github repository:",a("Developing Data Products - Peer Assessment Project; Shiny App",href="https://github.com/suilou/DataScience9_DevelopingDataProducts/app"))))
   
   ), 
   mainPanel(    
